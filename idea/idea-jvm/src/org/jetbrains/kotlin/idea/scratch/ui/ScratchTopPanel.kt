972832099ca1015906ce0d8b63e6ca21d8c6199b
Natalia Selezneva <natalia.selezneva@jetbrains.com>
2018-12-12 12:25:14 +0300

    Refactoring: move scratch options to ScratchFile from ScratchPanel

--- idea/idea-jvm/src/org/jetbrains/kotlin/idea/scratch/ui/ScratchTopPanel.kt
+++ idea/idea-jvm/src/org/jetbrains/kotlin/idea/scratch/ui/ScratchTopPanel.kt
@@ -69,27 +69,27 @@ class ScratchTopPanel private constructor(val scratchFile: ScratchFile) : JPanel
 
         add(JSeparator(SwingConstants.VERTICAL))
 
-        isReplCheckbox = JCheckBox("Use REPL", false)
+        isReplCheckbox = JCheckBox("Use REPL")
         add(isReplCheckbox)
         isReplCheckbox.addItemListener {
-            scratchFile.getPsiFile()?.virtualFile?.apply {
-                scratchPanelConfig = (scratchPanelConfig ?: ScratchPanelConfig()).copy(isRepl = isReplCheckbox.isSelected)
+            scratchFile.saveOptions {
+                copy(isRepl = isReplCheckbox.isSelected)
             }
         }
 
         add(JSeparator(SwingConstants.VERTICAL))
 
-        isMakeBeforeRunCheckbox = JCheckBox("Make before Run", false)
+        isMakeBeforeRunCheckbox = JCheckBox("Make before Run")
         add(isMakeBeforeRunCheckbox)
         isMakeBeforeRunCheckbox.addItemListener {
-            scratchFile.getPsiFile()?.virtualFile?.apply {
-                scratchPanelConfig = (scratchPanelConfig ?: ScratchPanelConfig()).copy(isMakeBeforeRun = isMakeBeforeRunCheckbox.isSelected)
+            scratchFile.saveOptions {
+                copy(isMakeBeforeRun = isMakeBeforeRunCheckbox.isSelected)
             }
         }
 
         add(JSeparator(SwingConstants.VERTICAL))
 
-        (scratchFile.getPsiFile()?.virtualFile?.scratchPanelConfig ?: ScratchPanelConfig()).let {
+        scratchFile.options.let {
             isReplCheckbox.isSelected = it.isRepl
             isMakeBeforeRunCheckbox.isSelected = it.isMakeBeforeRun
         }
@@ -110,9 +110,6 @@ class ScratchTopPanel private constructor(val scratchFile: ScratchFile) : JPanel
             }
         }
     }
-
-    fun isRepl() = isReplCheckbox.isSelected
-    fun isMakeBeforeRun() = isMakeBeforeRunCheckbox.isSelected
 
     @TestOnly
     fun setReplMode(isSelected: Boolean) {
@@ -143,5 +140,3 @@ class ScratchTopPanel private constructor(val scratchFile: ScratchFile) : JPanel
         }
     }
 }
-
-data class ScratchPanelConfig(val isRepl: Boolean = false, val isMakeBeforeRun: Boolean = false)
